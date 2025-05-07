df78bdd1920d4b6b4ae45e1a00136b198a2b4a95
Jorge Garduño <jorge@rem.tools>
2025-05-06 21:47:18 -0600

    chore: do not boot wine

--- start_server.sh
+++ start_server.sh
@@ -62,7 +62,7 @@ main() {
   echo " "
   # Start server
   v() {
-    bash -c "WINEPREFIX=~/.wine wineboot --init"
+    # bash -c "WINEPREFIX=~/.wine wineboot --init"
     hangover_cmd="$server_directory/VRisingServer.exe -persistentDataPath $server_data -logFile $server_data/$logfile -nographics -batchmode"
     bash -c "HODLL64=libarm64ecfex.dll HODLL=libwow64fex.dll wine '$hangover_cmd' 2>&1" &
     echo $!
