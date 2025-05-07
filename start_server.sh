c42f81e68cd1f2ccb960f26cb47d227193dfb566
Jorge Garduño <jorge@rem.tools>
2025-05-07 01:56:02 -0600

    chore: add wine prefix for 64 bit

--- start_server.sh
+++ start_server.sh
@@ -61,15 +61,15 @@ main() {
   echo "Preparing to launch V Rising Dedicated Server"
   echo " "
 
-  #  bash -c "WINEARCH=win64 wineboot --init"
-  #  bash -c "WINEARCH=win32 wineboot --init"
+    bash -c "WINEPREFIX=~/.wine64 WINEARCH=win64 wineboot --init"
+    bash -c "WINEPREFIX=~/.wine WINEARCH=win32 wineboot --init"
 
   echo "Launching V Rising Dedicated Server"
   echo " "
   # Start server
   v() {
     hangover_cmd="$server_directory/VRisingServer.exe -persistentDataPath $server_data -logFile $server_data/$logfile -nographics -batchmode"
-    bash -c "WINEARCH=win64 HODLL64=libarm64ecfex.dll HODLL=libwow64fex.dll wine '$hangover_cmd' 2>&1" &
+    bash -c "WINEPREFIX=~/.wine64 WINEARCH=win64 HODLL64=libarm64ecfex.dll HODLL=libwow64fex.dll wine '$hangover_cmd' 2>&1" &
     echo $!
   }
 
